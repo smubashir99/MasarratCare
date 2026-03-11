b842b963d26ded90a6e64609fa86cd4ab3e77c8b
Syed Mubashir Ahmed Hashmi <syedmubashirahmed1234@gmail.com>
2026-03-11 20:25:19 +0000

    adding init_db function with products and shades tables using foreign key relationship

--- backend/db.py
+++ backend/db.py
@@ -4,3 +4,29 @@ def get_db():
     conn = sqlite3.connect("backend/medora.db")
     conn.row_factory = sqlite3.Row
     return conn
+
+def init_db():
+    conn = get_db()
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS products (
+            id          INTEGER PRIMARY KEY AUTOINCREMENT,
+            name        TEXT NOT NULL,
+            category    TEXT,
+            price       REAL,
+            description TEXT
+        )
+    """)
+
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS shades (
+            id         INTEGER PRIMARY KEY AUTOINCREMENT,
+            product_id INTEGER,
+            shade_name TEXT NOT NULL,
+            hex_code   TEXT,
+            FOREIGN KEY (product_id) REFERENCES products(id)
+        )
+    """)
+
+    conn.commit()
+    conn.close()
