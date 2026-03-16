cef65d4a27e9b405daa840a1813c9af990589037
Syed Mubashir Ahmed Hashmi <syedmubashirahmed1234@gmail.com>
2026-03-16 15:21:45 +0000

    adding product API routes — GET /products, POST /products, PUT /products/<id>, DELETE /products/<id>

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,47 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from db import init_db
+from models import (
+    create_product, get_all_products, update_product, delete_product
+)
 
 app = Flask(__name__)
 CORS(app)
 init_db()
+
+#  PRODUCTS ROUTES
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(get_all_products())
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    data = request.get_json()
+    create_product(
+        data['name'],
+        data['category'],
+        data['price'],
+        data['description']
+    )
+    return jsonify({'message': 'Product created'}), 201
+
+@app.route('/products/<int:id>', methods=['PUT'])
+def edit_product(id):
+    data = request.get_json()
+    update_product(
+        id,
+        data['name'],
+        data['category'],
+        data['price'],
+        data['description']
+    )
+    return jsonify({'message': 'Product updated'})
+
+@app.route('/products/<int:id>', methods=['DELETE'])
+def remove_product(id):
+    delete_product(id)
+    return jsonify({'message': 'Product deleted'})
 
 #  PING (For Integration Testing)
 
