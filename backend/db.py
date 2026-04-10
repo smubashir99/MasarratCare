dbf3eed080a94aad457c8089119fc40d815b2927
Syed Mubashir Ahmed Hashmi <syedmubashirahmed1234@gmail.com>
2026-04-10 11:08:11 +0100

    Fix SQLite path in db.py for production: - Added dynamic BASE_DIR path resolution - Ensured medora.db works on Render and local system

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,11 @@
 import sqlite3
+import os
 
 def get_db():
-    conn = sqlite3.connect("medora.db")
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "medora.db")
+
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
 
