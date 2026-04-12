c1fd0a252b3471351a5769428a596696b904cb39
Syed Mubashir Ahmed Hashmi <syedmubashirahmed1234@gmail.com>
2026-04-12 19:36:32 +0100

    update name

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH  = os.path.join(BASE_DIR, "medora.db")
+DB_PATH  = os.path.join(BASE_DIR, "masarratcare.db")
 
 def get_db():
     conn = sqlite3.connect(DB_PATH)
